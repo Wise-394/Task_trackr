527ace7327c470759bbc09fbc2af8c06417c089c
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-07-05 15:36:50 +0800

    navigation bar now working

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
-      home: const HomeScaffold(),
+      home: const DefaultScaffold(),
     );
   }
 }
 
-class HomeScaffold extends StatelessWidget {
-  const HomeScaffold({
+class DefaultScaffold extends StatefulWidget {
+  const DefaultScaffold({
     super.key,
   });
+
+  @override
+  State<DefaultScaffold> createState() => _DefaultScaffoldState();
+}
+
+class _DefaultScaffoldState extends State<DefaultScaffold> {
+  int _selectedIndex = 0;
+  List<Widget> body = const [HomeStack(), Placeholder()];
+
+  _changeIndex(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,32 +48,48 @@ class HomeScaffold extends StatelessWidget {
         ),
         backgroundColor: colorScheme.primary,
       ),
-      body: const Column(children: [
-        Center(
-          child: Text('Welcome to To-do app'),
-        ),
-      ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("yay");
-        },
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: const BottomNavWidget(),
+      body: Center(child: body[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (int newIndex) {
+            _changeIndex(newIndex);
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.notes), label: 'Notes'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings')
+          ]),
     );
   }
 }
 
-class BottomNavWidget extends StatelessWidget {
-  const BottomNavWidget({
+class HomeStack extends StatelessWidget {
+  const HomeStack({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.notes), label: 'Notes'),
-      BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
-    ]);
+    return Stack(
+      children: [
+        const Column(
+          children: [
+            Center(
+              child: Text('Welcome to To-do app'),
+            ),
+          ],
+        ),
+        Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: FloatingActionButton(
+            onPressed: () {
+              print("yay");
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
+    );
   }
 }
