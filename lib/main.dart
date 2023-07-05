3e526cc271cb701efe33045293483fde0c79a13b
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-07-05 10:39:19 +0800

    init com

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('To-do app'),
-        ),
-        body: const Column(children: [
-          Center(
-            child: Text('Welcome to To-do app'),
-          ),
-        ]),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("yay");
-          },
-          child: const Icon(Icons.add),
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.notes), label: 'Notes'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
-        ]),
-      ),
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const HomeScaffold(),
     );
+  }
+}
+
+class HomeScaffold extends StatelessWidget {
+  const HomeScaffold({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'To-do app',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: colorScheme.primary,
+      ),
+      body: const Column(children: [
+        Center(
+          child: Text('Welcome to To-do app'),
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("yay");
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: const BottomNavWidget(),
+    );
+  }
+}
+
+class BottomNavWidget extends StatelessWidget {
+  const BottomNavWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: const [
+      BottomNavigationBarItem(icon: Icon(Icons.notes), label: 'Notes'),
+      BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+    ]);
   }
 }
