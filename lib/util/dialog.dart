b9c5fdd7cd7e0330b7fb3c227df7498600aecd0b
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-07-10 09:38:18 +0800

    Revert "idk what im doing anymore"
    This reverts commit c8d3ff5e59e1ccfa11e47227f6d1e83bc78611b9.

--- lib/util/dialog.dart
+++ lib/util/dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/util/button.dart';
 
-class DialogToDo extends StatelessWidget {
+class DialogToDo extends StatefulWidget {
   final controller;
   VoidCallback onSave;
   VoidCallback onCancel;
@@ -13,6 +13,11 @@ class DialogToDo extends StatelessWidget {
       required this.onSave});
 
   @override
+  State<DialogToDo> createState() => _DialogToDoState();
+}
+
+class _DialogToDoState extends State<DialogToDo> {
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('New Task'),
@@ -20,18 +25,18 @@ class DialogToDo extends StatelessWidget {
         shape: const RoundedRectangleBorder(),
         child: Column(mainAxisSize: MainAxisSize.min, children: [
           TextField(
-              controller: controller,
+              controller: widget.controller,
               decoration: const InputDecoration(
                   border: OutlineInputBorder(), labelText: 'enter task here')),
         ]),
       ),
       actions: <Widget>[
         BtnPrototype(
-          btnAction: onCancel,
+          btnAction: widget.onCancel,
           btnText: 'Cancel',
         ),
         BtnPrototype(
-          btnAction: onSave,
+          btnAction: widget.onSave,
           btnText: 'Save',
         ),
       ],
