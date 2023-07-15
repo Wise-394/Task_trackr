08107bcd003e187c992c3ece9d8051e1076eaedd
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-07-15 15:51:27 +0800

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,10 @@ import 'package:todo_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(
+      theme: ThemeMode.dark,
+      route: 'okay',
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
